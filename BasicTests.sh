b07ce840b8d87625a0e460f745449b5d595d8286
Suki <46114711+suki-sahota@users.noreply.github.com>
2022-01-01 10:21:07 -0800

    Uploads scripts to run test cases
    The script file runs all the test cases for an algorithm and produces an output file.

---
+++ BasicTests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+javac Basic_Alignment.java
+java Basic_Alignment TestCases/input1.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input2.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input3.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input4.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input5.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input6.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input7.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input8.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input9.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input10.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input11.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input12.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input13.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input14.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input15.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input16.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input17.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input18.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input19.txt >> BasicTestOutput.txt
+java Basic_Alignment TestCases/input20.txt >> BasicTestOutput.txt
---
+++ EfficientTests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+javac Efficient_Alignment.java
+java Efficient_Alignment TestCases/input1.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input2.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input3.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input4.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input5.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input6.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input7.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input8.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input9.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input10.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input11.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input12.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input13.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input14.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input15.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input16.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input17.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input18.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input19.txt >> EfficientTestOutput.txt
+java Efficient_Alignment TestCases/input20.txt >> EfficientTestOutput.txt
